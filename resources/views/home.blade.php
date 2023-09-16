d2ab8cb6f5627f5974fea1943cd62940f68d416c
carloeusebi <carloeusebi@gmail.com>
2023-09-16 17:53:42 +0200

    Improved paddings and width for smartphones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,24 +4,24 @@
 
     <main class="overflow-auto p-0 h-screen grow overflow-x-hidden">
 
-        <section id="jumbo" class="px-2" x-intersect:margin.4vh="activeLink = 'jumbo'; show = true"
-            x-intersect:leave="show = false" x-data="{ show: false }">
+        <section id="jumbo" x-intersect:margin.4vh="activeLink = 'jumbo'; show = true" x-intersect:leave="show = false"
+            x-data="{ show: false }">
             @include('partials.home.jumbo')
         </section>
 
-        <section id="about" class="px-2" x-intersect:margin.4vh="activeLink = 'about'">
+        <section id="about" x-intersect:margin.4vh="activeLink = 'about'">
             @include('partials.home.about')
         </section>
 
-        <section id="resumee" class="px-2" x-intersect:margin.4vh="activeLink = 'resumee'">
+        <section id="resumee" x-intersect:margin.4vh="activeLink = 'resumee'">
             @include('partials.home.resumee')
         </section>
 
-        <section id="portfolio" class="px-2" x-intersect:margin.4vh="activeLink = 'portfolio'">
+        <section id="portfolio" x-intersect:margin.4vh="activeLink = 'portfolio'">
             @include('partials.home.portfolio')
         </section>
 
-        <section id="contact" class="px-2" x-intersect:margin.4vh="activeLink = 'contact'">
+        <section id="contact" x-intersect:margin.4vh="activeLink = 'contact'">
             @include('partials.home.contact')
         </section>
 
